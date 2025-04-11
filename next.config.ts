089782455180e39b41865a5ed0fef914d8d634f8
Anand <ananddas2621@gmail.com>
2025-04-11 15:02:37 +0530

    config edited

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,61 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logo.svgcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.nordfabrik.ch',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'learnai.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'brandpalettes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i1.wp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'aipedia.co.il',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.g2crowd.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'techpilot.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: '10web.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logospng.org',
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
